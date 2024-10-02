b0eff6694b374804a0edd7fddf710335565249a5
mrjafari202 <m.r.jafari202@gmail.com>
2024-10-02 22:00:10 +0330

    complete actions

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,51 +1,79 @@
-import React, {  createContext, useContext, useReducer } from 'react'
+import React, { createContext, useContext, useReducer } from 'react'
 import { sumProducts } from '../helper/helper';
 
 const initialState = {
-    selectedItems : [],
-    itemsCounter : 0,
-    total : 0 ,
-    cheackout : false
+    selectedItems: [],
+    itemsCounter: 0,
+    total: 0,
+    cheackout: false
 };
 
-const reducer = (state , actions) => {
+const reducer = (state, actions) => {
     console.log(state)
-switch (actions.type) {
-    case "ADD_ITEM":
-        if(!state.selectedItems.find((item) => item.id === actions.payload.id))
-            state.selectedItems.push({...actions.payload ,quantity:1})
-        return {
-            ...state,
-            cheackout : false,
-            ...sumProducts(state.selectedItems)
-        }
-        
-       
+    switch (actions.type) {
+        case "ADD_ITEM":
+            if (!state.selectedItems.find((item) => item.id === actions.payload.id))
+                state.selectedItems.push({ ...actions.payload, quantity: 1 })
+            return {
+                ...state,
+                cheackout: false,
+                ...sumProducts(state.selectedItems)
+            }
 
-    default:
-       throw new Error("invalid action")
-}
+        case "REMOVE_ITEM":
+            const newSelectedItems = state.selectedItems.filter((item) => item.id !== actions.payload.id)
+            return {
+                ...state,
+                selectedItems: [...newSelectedItems],
+                ...sumProducts(newSelectedItems)
+            }
+
+        case "INCREASE":
+            const IncreaseIndex = state.selectedItems.findIndex(item => item.id === actions.payload.id);
+            state.selectedItems[IncreaseIndex].quantity++;
+            return {
+                ...state ,
+                ...sumProducts(state.selectedItems)
+            }
+        case "DECREASE":
+            const DecreaseIndex = state.selectedItems.findIndex(item => item.id === actions.payload.id);
+            state.selectedItems[DecreaseIndex].quantity--;
+            return {
+                ...state ,
+                ...sumProducts(state.selectedItems)
+            }
+
+            case "CHECKOUT" :
+                return {
+                    selectedItems : [],
+                    itemsCounter : 0 ,
+                    total : 0 ,
+                    cheackout : true 
+                }
+        default:
+            throw new Error("invalid action")
+    }
 };
 
 
 const CartContex = createContext();
 
-const CartProvider = ({children}) => {
+const CartProvider = ({ children }) => {
 
     const [state, dispatch] = useReducer(reducer, initialState)
 
     return (
-        <CartContex.Provider value={{state , dispatch}}>
+        <CartContex.Provider value={{ state, dispatch }}>
             {children}
         </CartContex.Provider>
     )
-    
+
 }
 
 const useCart = () => {
-  const {state , dispatch} =  useContext(CartContex)
-  return [state , dispatch]
+    const { state, dispatch } = useContext(CartContex)
+    return [state, dispatch]
 };
 
 export default CartProvider;
-export {useCart}
+export { useCart }
